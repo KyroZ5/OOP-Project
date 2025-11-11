0edd06b7e9bcfbaa3af1281d517500fc71283f14
grynkazusa <gianhakdog24@gmail.com>
2025-11-11 11:44:17 +0800

    folder added

---
+++ posSystem/POSMain.java
@@ -0,0 +1,11 @@
+package posSystem;
+import java.util.*;
+
+public class POSMain  {
+ public static void main(String[] args) {
+	 Scanner sc = new Scanner(System.in);
+	 System.out.print("Enter a Number: ");
+	 int num = sc.nextInt();
+	 System.out.println(num);
+ }
+}
---
+++ posSystem/Users.java
@@ -0,0 +1,5 @@
+package posSystem;
+
+public class Users {
+
+}
