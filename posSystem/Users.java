e248a8a655ad1a7edd01dc3421eeaec4f71ac0e0
KyroZ5 <seankevin0912@gmail.com>
2025-11-11 12:31:25 +0800

    up

--- posSystem/Users.java
+++ posSystem/Users.java
@@ -2,7 +2,7 @@ package posSystem;
 import java.io.*;
 import java.util.*;
 class Users {
-	ArrayList <Users> accts = new ArrayList<>();
+	ArrayList<Users> accts = new ArrayList<>();
 	private String username;
 	private String password; 
 	private String employeeName;
@@ -11,20 +11,24 @@ class Users {
 		this.password = password;
 		this.employeeName = employeeName;
 	}
-
+	public String getUsername() { 
+		return username; }
+    public String getPassword() { 
+    	return password; }
+    public String getEmployeeName() { 
+    	return employeeName; }
+    
 	public void loadUsersFromFile() throws FileNotFoundException, IOException {
 		
 		try(BufferedReader br = new BufferedReader(new FileReader("users.txt"))){
             String line;
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(",");
-                if (parts.length == 3) { 
-                    accts.add("hahaha").;
-                    username.add(parts[1]);
-                    password.add(parts[2]);
+                if (parts.length == 3) {
+                    Users user = new Users(parts[1], parts[2], parts[0]);
+                    accts.add(user);
                 }
             }
-            int size = username.size();
 		}
-	}
+	}	
 }
