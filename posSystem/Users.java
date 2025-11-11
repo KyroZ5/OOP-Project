70eb7faa3f0c1bf710aca49873dbaf536a285d5a
grynkazusa <gianhakdog24@gmail.com>
2025-11-11 11:50:43 +0800

    Update Users.java

--- posSystem/Users.java
+++ posSystem/Users.java
@@ -1,5 +1,30 @@
 package posSystem;
+import java.io.*;
+import java.util.*;
+class Users {
+	ArrayList <Users> accts = new ArrayList<>();
+	private String username;
+	private String password; 
+	private String employeeName;
+	Users(String username, String password, String employeeName){
+		this.username = username;
+		this.password = password;
+		this.employeeName = employeeName;
+	}
 
-public class Users {
-
+	public void loadUsersFromFile() throws FileNotFoundException, IOException {
+		
+		try(BufferedReader br = new BufferedReader(new FileReader("users.txt"))){
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 3) { 
+                    accts.add("hahaha").;
+                    username.add(parts[1]);
+                    password.add(parts[2]);
+                }
+            }
+            int size = username.size();
+		}
+	}
 }
